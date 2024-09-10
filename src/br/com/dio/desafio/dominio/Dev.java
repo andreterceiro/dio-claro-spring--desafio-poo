6abbc76c3d167580276c74cad2e3f8bdbd50caaf
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2024-09-10 13:57:43 -0300

    Implementing some methods related to business logic in the Dev class

--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -1,6 +1,7 @@
 package br.com.dio.desafio.dominio;
 
 import java.util.LinkedHashSet;
+import java.util.Optional;
 import java.util.Set;
 
 public class Dev {
@@ -9,15 +10,23 @@ public class Dev {
     private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
     
     public void inscreverBootcamp(Bootcamp bootcamp) {
-
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
     }
 
     public void progredir() {
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
 
+        if (conteudo.isPresent()) {
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());
+        } else {
+            System.err.println("Você naõ está matriculado em nenhum conteúdo");
+        }
     }
 
-    public void calcularTotalXP() {
-
+    public double calcularTotalXP() {
+        return this.conteudosConcluidos.stream().mapToDouble(Conteudo::calcularXP).sum();
     }
 
     public String getNome() {
